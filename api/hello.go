9cded246d47dc00c41f44a8d988afda57f8742b3
addisalemtafere <addisalem12@gmail.com>
2024-07-31 19:36:48 +0300

    minor modification

--- api/hello.go
+++ api/hello.go
@@ -2,9 +2,42 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+func hello() {
+	// URL of the Bank of Abyssinia exchange rates page
+	url := "https://www.bankofabyssinia.com/ExchangeRate"
+
+	// Make HTTP GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check if the request was successful
+	if resp.StatusCode != 200 {
+		log.Fatalf("Failed to get a valid response. Status Code: %d", resp.StatusCode)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to parse the response body: %v", err)
+	}
+
+	// Select the table using the provided XPath converted to CSS selectors
+	doc.Find("div > div > div > div > div > div > div > div > div > div > div > div > div > div > table").Each(func(i int, table *goquery.Selection) {
+		table.Find("tbody tr").Each(func(j int, row *goquery.Selection) {
+			currency := row.Find("td:nth-child(1)").Text()
+			buyRate := row.Find("td:nth-child(2)").Text()
+			sellRate := row.Find("td:nth-child(3)").Text()
+			
+			fmt.Printf("Currency: %s, Buy Rate: %s, Sell Rate: %s\n", currency, buyRate, sellRate)
+		})
+	})
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module template-go-vercel
 go 1.20
 
 require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	golang.org/x/net v0.24.0 // indirect
 )
